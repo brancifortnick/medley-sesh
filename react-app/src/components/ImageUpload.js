48b81558d250e079c457d823f7c85d9bd83cc05f
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-11-04 04:34:37 -0400

    added profile_img to imageupload

--- react-app/src/components/ImageUpload.js
+++ react-app/src/components/ImageUpload.js
@@ -6,12 +6,11 @@ import { getAllMusicians } from "../store/musician";
 
 const ImageUpload = () => {
   const history = useHistory();
-  const [profileImage, setImage] = useState(null);
+  const [profile_img, setImage] = useState(null);
   const [imageLoading, setImageLoading] = useState(false);
-  const musicians = useSelector(state=> Object.values(state.musician))
 
-  profileImage = musicians.profile_img;
-  console.log(musicians.profile_img)
+
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
