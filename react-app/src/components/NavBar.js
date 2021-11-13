7d667454c6e9b431cb25edf6dbdbe7644044d474
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-11-13 05:56:53 -0500

    added musicianId to Allsongs, MusicianRefactor, Navbar, User component, UsersMusicians added with a link to their added artists

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -4,6 +4,7 @@ import LogoutButton from "./auth/LogoutButton";
 import { useDispatch, useSelector } from "react-redux";
 import { demoLogin } from "../store/session";
 import "./NavBar.module.css";
+import UsersMusicians from "./UsersMusicians/UsersMusicians";
 
 const NavBar = () => {
   const user = useSelector((state) => state.session.user);
@@ -77,6 +78,7 @@ const NavBar = () => {
             style={{ textDecoration: "none" }}
           >
             {`${user.username}s Profile`}
+
           </NavLink>
         </button>
       ) : null}
