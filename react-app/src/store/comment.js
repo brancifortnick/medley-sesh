87438b41cdd3a603ddc26c621cebafeb2677f9df
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-11-13 22:16:52 -0500

    working on comment routes, comment component, comment store

--- react-app/src/store/comment.js
+++ react-app/src/store/comment.js
@@ -4,9 +4,9 @@ const DELETE_COMMENT = 'comment/DELETE_COMMENT';
 const EDIT_COMMENT = 'comment/EDIT_COMMENT';
 
 
-const getComments = (comment) => ({
+const getComments = (comments) => ({
     type: GET_COMMENTS,
-    payload: comment,
+    payload: comments,
 })
 
 const postComment = (comment) => ({
@@ -23,3 +23,30 @@ const editComment = (comment) => ({
     type: EDIT_COMMENT,
     payload: comment,
 })
+
+
+
+export const getAllComments = (song_id) => async (dispatch) => {
+    const response = await fetch(`/api/comments/${song_id}`)
+    if(response.ok){
+        const comments = await response.json();
+        dispatch(getComments(comments));
+    }
+};
+
+
+
+export default function reducer(state = initialState, action) {
+  let newState = {};
+  switch (action.type) {
+    case GET_COMMENTS:
+      action.payload.forEach((comment) => {
+        newState[comment.id] = comment;
+      });
+      return newState;
+    case ADD_COMMENT:
+        return {...action.payload}
+    default:
+        return state
+  }
+};
