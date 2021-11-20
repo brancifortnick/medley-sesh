3ceaf38449a943ed996f208024297d983ddc1f63
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-11-20 03:55:15 -0500

    clean up buttons musicianspage

--- react-app/src/components/AllSongs/AllSongs.js
+++ react-app/src/components/AllSongs/AllSongs.js
@@ -1,23 +1,21 @@
 import React, { useEffect } from "react";
-import {useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import DeleteTrack from "../DeleteTrack/DeleteTrack";
 import CommentDisplay from "../CommentDisplay/CommentDisplay";
-import './AllSongs.css'
+import "./AllSongs.css";
 import { getOneMusician } from "../../store/musician";
-import './AllSongs.css'
+import "./AllSongs.css";
 
 const AllSongs = ({ musicianId }) => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.session.user);
-  const songs = useSelector((state) =>  Object.values(state.song));
-  const comments = useSelector(state => Object.values(state.comment));
+  const songs = useSelector((state) => Object.values(state.song));
+  const comments = useSelector((state) => Object.values(state.comment));
 
-
-  let commentId = comments.map((comment)=> {
-    return songs.id === comment.song_id
-  })
-
+  let commentId = comments.map((comment) => {
+    return songs.id === comment.song_id;
+  });
 
   useEffect(() => {
     dispatch(getOneMusician(parseInt(musicianId)));
@@ -29,7 +27,7 @@ const AllSongs = ({ musicianId }) => {
       <>
         {songs.map((song) => (
           <div key={song.id} className="song-id">
-            <p className='title-p' >Title:{" "}{song.title} </p>
+            <p className="title-p">Title: {song.title} </p>
 
             <div id="audio-player">
               <audio src={song.file_url} controls></audio>
@@ -37,9 +35,6 @@ const AllSongs = ({ musicianId }) => {
                 <DeleteTrack musicianId={musicianId} songId={song.id} />
               ) : null}
             </div>
-            {/* {user.id === Number(song.musician_id) ? (
-              <CommentCreate musicianId={musicianId} songId={song.id} />
-            ) : null} */}
             <div id="comment-display-component">
               {user.id === Number(song.musician_id) ? (
                 <CommentDisplay
