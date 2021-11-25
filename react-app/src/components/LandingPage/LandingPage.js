bca00ab4f9dc8f70b5af28aa1b97b6f0ebfcd899
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-11-25 16:15:35 -0500

    fixed images displaying on background

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./LandingPage.css";
+import background from '../../assets/carsblur.jpg';
+
 
 const LandingPage = () => {
   return (
-    <>
+
+    <div className='landing-container'>
       <h1 id="welcome-text">Welcome To Medley</h1>
-    </>
+      <img src={background}></img>
+      </div>
+
   );
 };
 export default LandingPage;
