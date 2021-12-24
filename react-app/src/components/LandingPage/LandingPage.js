907bf14dafe9e80bd4b78c7cfcc025664ae367aa
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-12-23 21:02:41 -0500

    editing landing 2

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -6,8 +6,9 @@ const LandingPage = () => {
   return (
     <div className="landing-container">
       <h1 id="welcome-text">
-        Welcome to Medley, a place where you can upload your favorite musician,{" "}
+        Welcome to Medley, a place where you can upload your favorite musician(s),{" "}
         navigate to your artists' page and add their mp3s and other audio files tp enjoy!
+        Easily access all of your musicians by navigating to your profile page and choosing your desired artist.
       </h1>
       <img
         alt="_blank"
