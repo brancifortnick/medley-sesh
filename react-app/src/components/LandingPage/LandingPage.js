28056342a767e80ef2dc025e3d6c0812715d9a57
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-12-23 20:58:11 -0500

    editing landing page

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,19 @@
 import React from "react";
-import background from '../../assets/james-owen.jpg';
+import background from "../../assets/james-owen.jpg";
 import "./LandingPage.css";
-
 
 const LandingPage = () => {
   return (
     <div className="landing-container">
-      <h1 id='welcome-text'>Hello</h1>
-      <img alt ='_blank'className='background-image-landing'src={background}></img>
+      <h1 id="welcome-text">
+        Welcome to Medley, a place where you can upload your favorite musician,{" "}
+        navigate to your artists' page and add their mp3s and other audio files tp enjoy!
+      </h1>
+      <img
+        alt="_blank"
+        className="background-image-landing"
+        src={background}
+      ></img>
     </div>
   );
 };
