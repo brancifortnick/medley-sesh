027a047066b41adf01f466405148a60d602f52ae
Nicholas Brancifort <brancifortnick@yahoo.com>
2022-01-22 13:22:06 -0500

    fixed deletetrack and allsongs

--- react-app/src/components/AllSongs/AllSongs.js
+++ react-app/src/components/AllSongs/AllSongs.js
@@ -14,9 +14,11 @@ const AllSongs = ({ musicianId }) => {
   const dispatch = useDispatch();
 
   const user = useSelector((state) => state.session.user);
-  const songs = useSelector((state) => Object.values(state.song));
-  const musicians = useSelector((state) => Object.values(state.musician));
-
+  const songs = useSelector((state) => Object.values(state.song)); //array of obj with value of an obj
+  // const musicians = useSelector((state) => Object.values(state.musician));
+  const musician = useSelector(state => state.musician);
+  const song = useSelector(state => state.song)//obj with val of obj
+  console.log( user, "user>>>>",musician, "musicianS tate=>>>>>", song, "songSTATE-NO-OBJ", songs, "songs ----OBJECT")
   // const comment = useSelector((state) => state.comment);
   // console.log(comment, "<<<<comment");
   // const musician = useSelector((state) =>state.musician);
@@ -35,7 +37,7 @@ const AllSongs = ({ musicianId }) => {
             <p className="title-p">Title: {song.title} </p>
             <div id="audio-player">
               <audio src={song.file_url} controls></audio>
-              {user.id === Number(musicians.user_id) ? (
+              {user.id === Number(musician.user_id) ? (
                 <DeleteTrack musicianId={musicianId} songId={song.id} />
               ) : null}
             </div>
