c0a8de15b42a6a790a9f3e62c66cdea70c02f2d0
Nicholas Brancifort <brancifortnick@yahoo.com>
2022-01-22 14:27:46 -0500

    favicon.ico added:

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -7,7 +7,7 @@ const LandingPage = () => {
     <div className="landing-container">
       <h1 id="welcome-text">
         Welcome to Medley, a place where you can upload your favorite musician(s).
-        Navigate to your profile page to upload your favorite musicians mp3 and other audio files you would like to showcase.
+        Navigate to your profile page to upload artists and audio files you would like to showcase.
         Easily access all of your musicians by navigating to your profile page and choosing your desired artist.
       </h1>
       <img
